6e0aba62fc50953f592cffd1ee2817ce2568c8ac
Leandro Teixeira Wolff <leandro.teixeira.wolff@hotmail.com>
2024-03-31 19:13:05 -0300

    Add project files.

---
+++ DiamanteDeLetras.ConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+namespace DiamanteDeLetras.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma letra para desenhar o diamante:");
+            char letra = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            int tamanhoDoDiamante = letra - 'A' + 1;
+
+            for (int linha = 0; linha < tamanhoDoDiamante; linha++)
+            {
+                for (int coluna = 0; coluna < tamanhoDoDiamante - linha - 1; coluna++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write(Convert.ToChar('A' + linha));
+
+                for (int coluna = 0; coluna < 2 * linha - 1; coluna++)
+                {
+                    Console.Write(" ");
+                }
+
+                if (linha != 0)
+                {
+                    Console.Write(Convert.ToChar('A' + linha));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine(); 
+            }
+
+            for (int linha = tamanhoDoDiamante - 2; linha >= 0; linha--)
+            {
+                for (int coluna = 0; coluna < tamanhoDoDiamante - linha - 1; coluna++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write(Convert.ToChar('A' + linha));
+
+                for (int coluna = 0; coluna < 2 * linha - 1; coluna++)
+                {
+                    Console.Write(" ");
+                }
+
+                if (linha != 0)
+                {
+                    Console.Write(Convert.ToChar('A' + linha));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
+
